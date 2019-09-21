734c3f3a92f2f066bba9ff2d6af68aa9fc837c63
lwellausen <lwellausen@gmail.com>
2019-09-21 01:43:58 -0300

    replica essa merda

--- parte1.py
+++ parte1.py
@@ -7,13 +7,16 @@ def mouse_callback(event, x, y, flags, params):
         pos = (x, y)
 
         print(pos)
-
 img = cv2.imread('maracana1.jpg')
 cv2.namedWindow('image', cv2.WINDOW_NORMAL)
 cv2.resizeWindow('image', img.shape[1], img.shape[0])
 
-cv2.setMouseCallback('image', mouse_callback)
+k = 0
+#set mouse callback function for window
+while 1:
+    cv2.setMouseCallback('image', mouse_callback)
+    cv2.imshow('image', img)
+    k = cv2.waitKey(0)
 
-cv2.imshow('image', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    if (k == 27):
+        breakcv2.destroyAllWindows()
